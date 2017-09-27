3a819abf341e14b693b0a538445ab24e58a81a1e
Jing Qiu <aqiu0720@gmail.com>
2017-09-27 08:35:28 +0800

    Fix some logic problem in the game.

--- snake.py
+++ snake.py
@@ -3,9 +3,12 @@
 
 import curses
 import numpy as np
-import os as os
+import os
+import time
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
+
+KEY_ESC = 27
 
 curses.initscr()
 
@@ -21,16 +24,18 @@ win.nodelay(1)
 
 key = KEY_RIGHT                                                    # Initializing values
 score = 0
+key_conflict = {KEY_UP: KEY_DOWN, KEY_DOWN: KEY_UP, KEY_RIGHT: KEY_LEFT, KEY_LEFT: KEY_RIGHT}
 
 snake = [[4,10], [4,9], [4,8]]                                     # Initial snake co-ordinates
 food = [10,20]                                                     # First food co-ordinates
 
 win.addch(food[0], food[1], '*')                                   # Prints the food
+paused = False
 
+# numpy data collection initialization
 m = 5000
 count = 0
 keymap = {KEY_UP: 0, KEY_DOWN: 1, KEY_RIGHT: 2, KEY_LEFT: 3}
-
 x_data = np.zeros(shape=(win_height, win_width, m))
 y_data = np.zeros(shape=(4, m))
 
@@ -42,35 +47,57 @@ def collect_data(win, operation, count):
     key_index = keymap[operation]
     y_data[key_index][count] = operation
 
-while key != 27:                                                   # While Esc key is not pressed
+while True:                                                   # While Esc key is not pressed
     win.border(0)
     win.addstr(0, 2, 'Score : ' + str(score) + ' ')                # Printing 'Score' and
     win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
-    win.timeout(150 - int(len(snake)/5 + len(snake)/10) % 120)          # Increases the speed of Snake as its length increases
+    # Increases the speed of Snake as its length increases
+    timeout = 150 - (int(len(snake)/5 + len(snake)/10) % 120)
+    win.timeout(timeout)
 
     if count >= m:
         break
 
     prevKey = key                                                  # Previous key pressed
     event = win.getch()
-    key = key if event == -1 else event
+    if event == -1:
+        key = key
+    else:
+        key = event
+        time.sleep(timeout/1000.0)
+
+    if key == KEY_ESC:
+        break
 
     if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
-        key = -1                                                   # one (Pause/Resume)
-        while key != ord(' '):
-            key = win.getch()
+        paused = not paused
         key = prevKey
+
+    if paused:
         continue
 
-    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
+    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN]:     # If an invalid key is pressed
         key = prevKey
-
-    if key != 27:
+    else:
         collect_data(win, key, count)
+        if prevKey == key_conflict[key]:
+            key = prevKey
+
 
     # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
     # This is taken care of later at [1].
-    snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
+    dx = 0
+    dy = 0
+    if key == KEY_DOWN:
+        dy = 1
+    elif key == KEY_UP:
+        dy = -1
+    elif key == KEY_RIGHT:
+        dx = 1
+    elif key == KEY_LEFT:
+        dx = -1
+
+    snake.insert(0, [snake[0][0] + dy, snake[0][1] + dx])
 
     # If snake crosses the boundaries, make it enter from the other side
     if snake[0][0] == 0: snake[0][0] = win_height - 2
@@ -98,11 +125,13 @@ while key != 27:                                                   # While Esc k
     win.addch(snake[0][0], snake[0][1], '#')
     count = count + 1
 
-print("\nScore - " + str(score))
-print("http://bitemelater.in\n")
 curses.endwin()
 
-os.mkdir("data")
+print("\nScore - " + str(score))
+print("http://bitemelater.in\n")
+
+if not os.path.exists("data"):
+    os.makedirs("data")
 np.save('./data/screen.npy', x_data[:, :, 0:count-1])
 np.save('./data/operation.npy', y_data[:, 0:count-1])
 
