bea9ccfa51a51869d0b69e49533df16f8a81b74c
Jing Qiu <aqiu0720@gmail.com>
2017-09-28 15:49:05 +0800

    Initial rnn, not compilable.

--- play_snake.py
+++ play_snake.py
@@ -1,64 +1,80 @@
 import tensorflow as tf
+from tensorflow.contrib import rnn
 import numpy as np
 
 # Network Parameters
-n_hidden_1 = 20 # 1st layer number of neurons
-n_hidden_2 = 10 # 2nd layer number of neurons
-n_out = 4 # play snake total operations (KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT)
+n_hidden = 50 # 1st layer number of neurons
+n_classes = 4 # play snake total operations (KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT)
+learning_rate = 0.001
+training_steps = 10000
+timesteps = 2
 
-def main(_):
-    # Import data
-    X = np.load("./data/screen.npy")
-    Y = np.load("./data/operation.npy")
-    total_size = X.shape[0]
-    n_input = X.shape[1] * X.shape[2]
+# Import data
+x = np.load("./data/screen.npy")
+y = np.load("./data/operation.npy")
+total_size = x.shape[0]
+n_input = x.shape[1] * x.shape[2]
 
-    # Reshape X and Y to be a 2D array, for X with each row to be features
-    X = X.flatten().reshape(total_size, -1)
+# Reshape X to be a 2D array, for X with each row to be features
+x = x.flatten().reshape(total_size, -1)
 
-    # Setup train data and test data
-    train_size = int(total_size * 0.6)
-    x_train = X[0 : train_size - 1, :]
-    y_train = Y[0 : train_size - 1, :]
-    x_test = X[train_size : total_size - 1, :]
-    y_test = Y[train_size : total_size - 1, :]
+# Setup train data and test data
+train_size = int(total_size * 0.6)
+x_train = x[0 : train_size - 1, :]
+y_train = y[0 : train_size - 1, :]
+x_test = x[train_size : total_size - 1, :]
+y_test = y[train_size : total_size - 1, :]
 
-    # Create the model
-    x = tf.placeholder(tf.float32, [None, n_input])
-    W1 = tf.Variable(tf.random_normal([n_input, n_hidden_1]))
-    b1 = tf.Variable(tf.random_normal([n_hidden_1]))
-    layer_1 = tf.nn.relu(tf.add(tf.matmul(x, W1), b1))
+# tf Graph input
+X = tf.placeholder("float", [None, timesteps, n_input])
+Y = tf.placeholder("float", [None, n_classes])
 
-    W2 = tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]))
-    b2 = tf.Variable(tf.random_normal([n_hidden_2]))
-    layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, W2), b2))
+# Define weights
+weights = {
+    'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
 
-    W3 = tf.Variable(tf.random_normal([n_hidden_2, n_out]))
-    b3 = tf.Variable(tf.random_normal([n_out]))
-    y = tf.matmul(layer_2, W3) + b3
+def RNN(x, weights, biases):
+    # Prepare data shape to match `rnn` function requirements
+    # Current data input shape: (batch_size, timesteps, n_input)
+    # Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
 
-    # Define loss and optimizer
-    y_ = tf.placeholder(tf.float32, [None, 4])
+    # Unstack to get a list of 'timesteps' tensors of shape (batch_size, n_input)
+    x = tf.unstack(x, timesteps, 1)
 
-    cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    # Define a lstm cell with tensorflow
+    lstm_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
 
-    optimizer = tf.train.GradientDescentOptimizer(0.02)
-    train_step = optimizer.minimize(cross_entropy)
-    sess = tf.InteractiveSession()
-    tf.global_variables_initializer().run()
+    # Get lstm cell output
+    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
 
-    # Train
-    for epoch in range(15000):
-        _, loss = sess.run([train_step, cross_entropy], feed_dict={x: x_train, y_: y_train})
-        if epoch % 100 == 0:
-            print("Epoch:", '%04d' % epoch, "loss={:.9f} ".format(loss))
+    # Linear activation, using rnn inner loop last output
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
-    # Test trained model
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print(sess.run(accuracy, feed_dict={x: x_test,
-                                        y_: y_test}))
 
-if __name__ == '__main__':
-    tf.app.run(main=main)
+logits = RNN(X, weights, biases)
+prediction = tf.nn.softmax(logits)
+
+loss_op = tf.reduce_mean(
+tf.nn.softmax_cross_entropy_with_logits(labels=y_train, logits=logits))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+train_step = optimizer.minimize(loss_op)
+
+correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+# Train
+for epoch in range(training_steps):
+    x_train = x_train.reshape(x_train.shape[0], timesteps, n_input)
+    _, loss = sess.run([train_step, loss_op], feed_dict={X: x_train, Y: y_train})
+    if epoch % 100 == 0:
+        print("Epoch:", '%04d' % epoch, "loss={:.9f} ".format(loss))
+
+x_test = x_test.reshape(x_test.shape[0], timesteps, n_input)
+print(sess.run(accuracy, feed_dict={X: x_test, Y: y_test}))
